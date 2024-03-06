ffd1fd30bba98aa0876421a6f9adf7c427d5b5be
William Mills <wmpmills@gmail.com>
2024-03-06 15:57:48 +0000

    fix: system volume update handler

--- webapp/script.js
+++ webapp/script.js
@@ -91,7 +91,7 @@ class DevicePlayerControl {
         this.xapi.Status.Audio.Volume.on(this.proccessVolumeChange.bind(this));
         
         this.xapi.Status.Audio.Volume.get()
-        .then(value=>this.proccessVolumeChange(value).bind(this))
+        .then(this.proccessVolumeChange.bind(this))
       });
 
     const options = {
